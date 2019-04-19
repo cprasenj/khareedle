b81bdb33f41b4a59c5f7596ca186a4134c1f8abc
Prasenjit Chakraborty <cprasenj@thoughtworks.com>
2019-04-19 19:12:24 +0530

    JSON import

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,19 @@
 import * as Koa from 'koa'
 import * as Router from 'koa-router' 
+const json = require('koa-json')
+
 
 const app = new Koa()
 const router = new Router()
 
+
+app.use(json())
+
 router.get('khareedle', '/', (ctx) => {
-  ctx.body = "<H1>khareedle</H1>"
+  ctx.body = { foo: 'bar' }
 })
 
 app.use(router.routes())
   .use(router.allowedMethods())
 
-app.listen(process.env.PORT ||3000, () => console.log('running on port 3000'))
+app.listen(process.env.PORT || 3000, () => console.log('running on port 3000'))
