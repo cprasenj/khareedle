3b4a78bf93ab4e8b98e4f36f2352671f09c47a20
Abhisek Harsh <abhisek.harsh@in-abhisek.harsh>
2019-04-19 11:53:38 +0530

    Converted the project as typescript project

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import * as Koa from 'koa'
+import * as Router from 'koa-router' 
+
+const app = new Koa()
+const router = new Router()
+
+router.get('khareedle', '/', (ctx) => {
+  ctx.body = "<H1>khareedle</H1>"
+})
+
+app.use(router.routes())
+  .use(router.allowedMethods())
+
+app.listen(3000, () => console.log('running on port 3000'))
